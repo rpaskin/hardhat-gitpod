a7363ae3342c36e2d81291645768ac6d253ddfd8
Ronnie Paskin <ronnie@megaputz.com>
2024-09-03 18:31:03 +0000

    Simplifica config

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,25 +11,26 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-// const alchemy_url = "https://eth-sepolia.g.alchemy.com/v2/...";
+const alchemy_url = "https://eth-sepolia.g.alchemy.com/v2/YGmjo8mVFH_BxgwE-OFn3KMgiVxG1kwW";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
-  defaultNetwork: "hardhat",
+  defaultNetwork: "sepolia",
   networks: {
     hardhat: {
+      
     },
-    // sepolia: {
-    //   url: process.env.ALCHEMY_API_URL,
-    //   accounts: [`0x${process.env.PRIVATE_KEY}`],
-    //   chainId: 11155111,
-    // },
-    // mumbai: {
-    //   url: process.env.ALCHEMY_API_URL,
-    //   accounts: [`0x${process.env.PRIVATE_KEY}`],
-    //   // chainId: 80001, // Mumbai Testnet chain ID
-    //   chainId: 80002, // Amoy testnet
-    // }
+    sepolia: {
+      url: process.env.ALCHEMY_API_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      chainId: 11155111,
+    },
+    mumbai: {
+      url: process.env.ALCHEMY_API_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+      // chainId: 80001, // Mumbai Testnet chain ID
+      chainId: 80002, // Amoy testnet
+    }
   }
 };
