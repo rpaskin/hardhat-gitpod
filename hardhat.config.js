3a3b09ce7ea56bb49d4e85ab2a874375a89e4c68
Ronnie Paskin <ronnie@megaputz.com>
2024-10-02 17:58:55 -0300

    Ajusta hardhat.config.js
    Signed-off-by: Ronnie Paskin <ronnie@megaputz.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,8 +11,6 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
-const alchemy_url = "https://eth-sepolia.g.alchemy.com/v2/YGmjo8mVFH_BxgwE-OFn3KMgiVxG1kwW";
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.20",
